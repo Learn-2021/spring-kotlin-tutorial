54d4ae3d231355636b363634f298fbd2faeeb12f
cocodori <moradulee@gmail.com>
2021-07-12 22:53:42 +0900

    Example Spring Docs

---
+++ src/test/kotlin/com/example/blog/RepositoriesTests.kt
@@ -0,0 +1,35 @@
+package com.example.blog
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.data.repository.findByIdOrNull
+
+@DataJpaTest
+class RepositoriesTest @Autowired constructor(
+    val entityManager: TestEntityManager,
+    val userRepository: UserRepository,
+    val articleRepository: ArticleRepository
+) {
+    @Test
+    fun `findByIdOrNull일 때 Article 반환`() {
+        val juergen = User("springjuergen", "Juergen", "Hoeller")
+        entityManager.persist(juergen)
+        val article = Article("Spring Framework 5.0 goes GA", "Dear Spring Community ...", "Lorem ipsum", juergen)
+        entityManager.persist(article)
+        entityManager.flush()
+        val found = articleRepository.findByIdOrNull(article.id!!)
+        assertThat(found).isEqualTo(article)
+    }
+
+    @Test
+    fun `findByLogin 호출할 때 User 반환`() {
+        val juergen = User("springjuergen", "Juergen", "Hoeller")
+        entityManager.persist(juergen)
+        entityManager.flush()
+        val user = userRepository.findByLogin(juergen.login)
+        assertThat(user).isEqualTo(juergen)
+    }
+}
